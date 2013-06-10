837b5370ffffa1e377d38463c37d2258d46f746c
Michael Johnston <lastobelus@mac.com>
2013-06-09 20:22:37 -0700

    add batch#over_limit?

--- lib/shydra/batch.rb
+++ lib/shydra/batch.rb
@@ -1,5 +1,6 @@
 module Shydra
   class Batch
+    OVER_LIMIT_RESPONSE_CODE = 429
 
     attr_accessor :requests
     def initialize
@@ -9,7 +10,11 @@ module Shydra
     delegate :<<, :size, :clear,  to: :requests
 
     def finished?
-      requests.all?{|r| ! r.response.nil?}
+      requests.all?{|r| !r.response.nil?}
+    end
+
+    def over_limit?
+      requests.any? { |r| r.response && (r.response.code == OVER_LIMIT_RESPONSE_CODE) }
     end
   end
 end
--- spec/shydra/batch_spec.rb
+++ spec/shydra/batch_spec.rb
@@ -7,22 +7,59 @@ describe Shydra::Batch do
     expect(subject.requests).to include(request)
   end
 
+  def request_and_response_mock(request_name, response_name=nil, response={})
+    req = double(request_name)
+    resp = response_name.nil? ? nil : double(response_name)
+    if response[:code]
+      resp.should_receive(:code).at_least(:once).and_return(response[:code])
+    else
+      resp.stub(:code).and_return(200) unless resp.nil?
+    end
+    if resp
+      req.should_receive(:response).at_least(:once).and_return(resp)
+    else
+      req.stub(:response).and_return(resp)
+    end
+    req
+  end
+
   describe "#finished?" do
     it "returns true when all the requests have a response" do
-      r1 = double("request 1"); r1.should_receive(:response).and_return(double('response 1'))
-      r2 = double("request 2"); r2.should_receive(:response).and_return(double('response 2'))
+      r1 = request_and_response_mock("request 1", 'response 1')
+      r2 = request_and_response_mock("request 2", 'response 2')
       subject << r1; subject << r2
       expect(subject.finished?).to be_true
     end
 
     it "returns false when not all the requests have a response" do
-      r1 = double("request 1"); r1.should_receive(:response).and_return(double('response 1'))
-      r2 = double("request 2"); r2.should_receive(:response).and_return(nil)
+      r1 = request_and_response_mock("request 1", 'response 1')
+      r2 = request_and_response_mock("request 2", nil)
       subject << r1; subject << r2
       expect(subject.finished?).to be_false
-    end
-    
+    end    
   end
+
+  describe "#over_limit?" do
+    before do
+    end
+    it "returns false when all the requests do not have response code 429" do
+      r1 = request_and_response_mock("request 1", 'response 1', code: 200)
+      r2 = request_and_response_mock("request 2", 'response 2', code: 500)
+      subject << r1; subject << r2
+      subject << r1; subject << r2
+      expect(subject.over_limit?).to be_false
+    end
+
+    it "returns true when any request has response code 429" do
+      r1 = request_and_response_mock("request 1", 'response 1', code: 200)
+      r2 = request_and_response_mock("request 2", 'response 2', code: 429)
+      r3 = request_and_response_mock("request 3") # over_limit will return true after finding the first 429
+      subject << r1; subject << r2; subject << r3
+      expect(subject.over_limit?).to be_true
+    end    
+  end
+
+  
 
 
 end
